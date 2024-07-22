f80cdfeb345eae2588935bfe5107f212efff9615
unknown <matumasie@gmail.com>
2024-07-22 03:06:19 +0000

    Add Index.jsx

---
+++ app/frontend/pages/Dashboard/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header';
+
+export default function Index() {
+  return ( 
+    <div className='w-full bg-white p-4 rounded-lg'>
+      <Header />
+    </div>
+   );
+}
+
